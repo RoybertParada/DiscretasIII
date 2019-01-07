b6db5503d8bf4454701199d61076fff994bed10a
Roybert Parada <paradaroybert@gmail.com>
2019-01-07 16:28:40 -0400

    Inicio del proyecto, creando la lectura de la entrada

--- Tarea2/src/main/java/Tarea2/Main/App.java
+++ Tarea2/src/main/java/Tarea2/Main/App.java
@@ -1,13 +1,69 @@
 package Tarea2.Main;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class App{
+	
+	static ArrayList<Namekiano> Namekianos = new ArrayList<Namekiano>();
+	
+    public static void main( String[] args ) throws NumberFormatException, IOException{
+    	BufferedReader filein = new BufferedReader( new FileReader( "tarea2.in" ) );
+    	int casos = Integer.parseInt(filein.readLine());
+    	while(casos>0){
+    		while(true){
+    			System.out.println("Infinite :v");
+    			String nombres = filein.readLine();
+    			if(nombres.length()==1)
+    				break;
+    			String nombres_separados[] = nombres.split(" ");
+    			
+    			if(Namekianos.isEmpty()){
+    				System.out.println("Namekiano Firts");
+    				Namekiano Padre = new Namekiano();
+    				Namekiano Hijo = new Namekiano();
+    				Padre.Nombre=nombres_separados[0];
+    				Hijo.Nombre=nombres_separados[1];
+    				Hijo.Padre=Padre;
+    				ArrayList<Namekiano> Hijos = new ArrayList<Namekiano>();
+    				Hijos.add(Hijo);
+    				Padre.Hijos = Hijos;
+    				Namekianos.add(Padre);
+    				Namekianos.add(Hijo);
+    			}else{
+    				System.out.println("Namekiano In");
+    				Namekiano Padre = new Namekiano();
+    				Namekiano Hijo = new Namekiano();
+    				for(int i = 0; i < Namekianos.size(); i++){
+    					System.out.println("Namekiano For");
+    					if(Namekianos.get(i).Nombre==nombres_separados[0]){
+    						Hijo.Nombre=nombres_separados[1];
+    	    				Hijo.Padre = Namekianos.get(i);
+    						ArrayList<Namekiano> Hijos = new ArrayList<Namekiano>();
+    						Hijos = Namekianos.get(i).Hijos;
+    						Hijos.add(Hijo);
+    					}else {
+    						Padre.Nombre=nombres_separados[0];
+    	    				Hijo.Nombre=nombres_separados[1];
+    	    				Hijo.Padre=Padre;
+    	    				ArrayList<Namekiano> Hijos = new ArrayList<Namekiano>();
+    	    				Hijos.add(Hijo);
+    	    				Padre.Hijos = Hijos;
+    	    				Namekianos.add(Padre);
+    	    				Namekianos.add(Hijo);
+    					}
+    				}
+    			}
+    		}
+    		casos--;
+    	}
+    	
+    	for(int i =0; i < Namekianos.size(); i++){
+    		System.out.println(Namekianos.get(i).Nombre);
+    	}
+    	
+    	filein.close();
     }
 }
